f7b2e4dd764ad7abe2f8a911c77dbc41f85b8ca5
Jordan <jordanleelong5@gmail.com>
2020-07-25 18:46:42 -0700

    initial commit

---
+++ src/components/Clock.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+
+
+export default class Clock extends Component {
+
+    render() {
+        return (
+            <div className="clock-div">
+                <h1>Digital Clock</h1>
+            </div>
+        )
+    }
+
+}
---
+++ src/components/CurrentTime.js
@@ -0,0 +1,15 @@
+
+export default class CurrentTime extends Component {
+
+    getCurrentTime() {
+        
+    }
+
+    render() {
+        return (
+            this.getCurrentTime()
+        )
+    }
+
+
+}
