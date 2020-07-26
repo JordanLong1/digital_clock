1a459e21ec2b351d76e49c23d52d6fd2ebd04305
Jordan <jordanleelong5@gmail.com>
2020-07-25 19:38:47 -0700

    can get current date and time in getCurrentTime()

--- src/components/CurrentTime.js
+++ src/components/CurrentTime.js
@@ -1,15 +1,28 @@
+import React, { Component }  from 'react';
 
 export default class CurrentTime extends Component {
 
-    getCurrentTime() {
-        
-    }
-
+    
     render() {
         return (
-            this.getCurrentTime()
-        )
-    }
+    
+            <div className="current-time">
+                {this.getCurrentTime()}
+            </div>
+            )
+        }
+
+     getCurrentTime = () => {
+ 
+         let today = new Date(); 
+         let hours = today.getHours();
+         let minutes = today.getMinutes()
+         let seconds = today.getSeconds()
+
+         let currentTimeOfDay = hours + ":" + minutes + ":" + seconds
+
+         console.log(currentTimeOfDay)
+     }
 
 
 }
