f41574c411dfca4ec37d0ea30860a707470103ec
Jordan <jordanleelong5@gmail.com>
2020-07-25 19:38:10 -0700

    added <CurrentTime />

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Clock from './components/Clock.js'
+import CurrentTime from './components/CurrentTime.js'
 
 function App() {
   return (
   
     <div className="App">
       <Clock />
+      <CurrentTime />
     </div>
   );
 }
