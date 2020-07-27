417def27681d7816f999acea7ebf20f88031a590
Jordan <jordanleelong5@gmail.com>
2020-07-26 20:46:59 -0700

    added constructor, component mounts, and ticktock func now the clock is updating every second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Clock from './components/Clock.js'
-import CurrentTime from './components/CurrentTime.js'
+// import CurrentTime from './components/CurrentTime.js'
 
 function App() {
   return (
   
     <div className="App">
       <Clock />
-      <CurrentTime />
     </div>
   );
 }
